277cf9680085913cc5a1fe55b6eb51631e27d249
Rick_muda <Rickambergen25@gmail.com>
2024-07-02 11:26:28 +0200

    Saves page changes

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,5 +1,3 @@
-// app/routes/projects.tsx
-
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData, Link } from '@remix-run/react';
