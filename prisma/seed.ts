15daa47e778c9ba2fc45e48ee5a30e923fb3d81b
Rick_muda <Rickambergen25@gmail.com>
2024-07-03 14:28:54 +0200

    Seeding fix "npm run seed"

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,30 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const seedData = [
-    {
-      id: 1,
-      username: "rick_muda",
-      password: "Rick23042002",
-      email: "rickambergen25@gmail.com",
-      is_admin: 1
-    }
+  {
+    username: 'rick_muda',
+    password: 'Rick23042002',
+    email: 'rickambergen25@gmail.com',
+    is_admin: 1,
+  },
 ];
 
 async function seed() {
-    const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-    try {
-        for (const user of seedData) {
-            await prisma.users.create({
-                data: user,
-            });
-        }
-
-        console.log("Seed data inserted successfully");
-    } catch (error) {
-        console.error("Error inserting seed data", error);
-    } finally {
-        await prisma.$disconnect();
+  try {
+    for (const user of seedData) {
+      await prisma.users.create({
+        data: user,
+      });
     }
+
+    console.log('Seed data inserted successfully');
+  } catch (error) {
+    console.error('Error inserting seed data', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 seed();
